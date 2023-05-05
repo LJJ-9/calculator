370f69bbc5b0e85ee97c8396417abca6089aa552
LJJ <ljjluckystar@163.com>
2023-05-05 10:09:47 +0800

    Add files via upload
    写一个计算器类（Calculator），可以实现两个数的加、减、乘、除运算，并可以进行undo和redo操作，侯选人可在实现功能的基础上发挥最优设计

---
+++ src/calculator/Calculator.java
@@ -0,0 +1,213 @@
+package calculator;
+
+import java.util.Stack;
+
+/**
+ * 使用了命令模式来封装加、减、乘、除操作。每个运算都被封装在一个实现了Command接口的内部类中。
+ *
+ * 同时使用了两个栈，undoStack和redoStack来存储命令对象。
+ * 每当执行一个新命令时，我们将其添加到undoStack中，并清空redoStack。
+ * 当执行撤销操作（undo）时，我们从undoStack中弹出最近的命令，并执行其undo()方法，然后将它推到redoStack中。
+ * 当执行重做操作（redo）时，我们从redoStack中弹出最近的命令，并执行其execute()方法，然后将它推回undoStack中。
+ *
+ * getCurrentValue()方法返回当前计算器的值。
+ *
+ * @author ljj
+ * @date 2023/5/5
+ *
+ */
+public class Calculator {
+
+    /**
+     * 当前最新值，相当于计算器按=符号后的值
+     */
+    private double currentValue;
+    private Stack<Command> undoStack = new Stack<>();
+    private Stack<Command> redoStack = new Stack<>();
+
+    /**
+     * 加运算
+     * @param value
+     */
+    public void add(double value) {
+        executeCommand(new AddCommand(value));
+    }
+
+    /**
+     * 减运算
+     * @param value
+     */
+    public void subtract(double value) {
+        executeCommand(new SubtractCommand(value));
+    }
+
+    /**
+     * 乘运算
+     * @param value
+     */
+    public void multiply(double value) {
+        executeCommand(new MultiplyCommand(value));
+    }
+
+    /**
+     * 除运算
+     * @param value
+     */
+    public void divide(double value) {
+        executeCommand(new DivideCommand(value));
+    }
+
+    /**
+     * 撤销
+     */
+    public void undo() {
+        if (!undoStack.empty()) {
+            Command command = undoStack.pop();
+            command.undo();
+            redoStack.push(command);
+        }
+    }
+
+    /**
+     * 重做
+     */
+    public void redo() {
+        if (!redoStack.empty()) {
+            Command command = redoStack.pop();
+            command.execute();
+            undoStack.push(command);
+        }
+    }
+
+    public double getCurrentValue() {
+        return currentValue;
+    }
+
+    /**
+     * 运算操作（加减乘除）
+     * @param command
+     */
+    private void executeCommand(Command command) {
+        command.execute();
+        undoStack.push(command);
+        redoStack.clear();
+    }
+
+    /**
+     * 接口类
+     */
+    private interface Command {
+        void execute();
+
+        void undo();
+    }
+
+    /**
+     * 加操作的实现类
+     */
+    private class AddCommand implements Command {
+        private final double operand;
+
+        public AddCommand(double operand) {
+            this.operand = operand;
+        }
+
+        @Override
+        public void execute() {
+            currentValue += operand;
+        }
+
+        @Override
+        public void undo() {
+            currentValue -= operand;
+        }
+    }
+
+    /**
+     * 减操作的实现类
+     */
+    private class SubtractCommand implements Command {
+        private final double operand;
+
+        public SubtractCommand(double operand) {
+            this.operand = operand;
+        }
+
+        @Override
+        public void execute() {
+            currentValue -= operand;
+        }
+
+        @Override
+        public void undo() {
+            currentValue += operand;
+        }
+    }
+
+    /**
+     * 乘操作的实现类
+     */
+    private class MultiplyCommand implements Command {
+        private final double operand;
+
+        public MultiplyCommand(double operand) {
+            this.operand = operand;
+        }
+
+        @Override
+        public void execute() {
+            currentValue *= operand;
+        }
+
+        @Override
+        public void undo() {
+            currentValue /= operand;
+        }
+    }
+
+    /**
+     * 除操作的实现类
+     */
+    private class DivideCommand implements Command {
+        private final double operand;
+
+        public DivideCommand(double operand) {
+            this.operand = operand;
+        }
+
+        @Override
+        public void execute() {
+            if (operand == 0) {
+                throw new IllegalArgumentException("Cannot divide by zero");
+            }
+            currentValue /= operand;
+        }
+
+        @Override
+        public void undo() {
+            currentValue *= operand;
+        }
+    }
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        // 第一次操作运算
+        calculator.add(8);
+        // 打印第一次操作运算后的最新值
+        System.out.println(calculator.getCurrentValue());
+
+        // 第一次操作撤销
+        calculator.undo();
+        // 打印第一次操作撤销后的最新值
+        System.out.println(calculator.getCurrentValue());
+
+        // 第一次操作重做
+        calculator.redo();
+        //打印第一次操作重做后的最新值
+        System.out.println(calculator.getCurrentValue());
+
+        // 再次操作撤销
+        calculator.undo();
+        System.out.println(calculator.getCurrentValue());
+    }
+}
